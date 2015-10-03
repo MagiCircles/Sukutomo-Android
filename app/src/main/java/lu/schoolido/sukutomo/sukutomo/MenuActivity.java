dae36e9531cab4c0a413fe416ef074128d9ca0a3
Arukantara <jose.alcantaramunoz@alum.uca.es>
2015-10-03 12:35:52 +0200

    Added update checking and upload.

--- app/src/main/java/lu/schoolido/sukutomo/sukutomo/MenuActivity.java
+++ app/src/main/java/lu/schoolido/sukutomo/sukutomo/MenuActivity.java
@@ -1,10 +1,13 @@
 package lu.schoolido.sukutomo.sukutomo;
 
 import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -15,8 +18,19 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MenuActivity extends ActionBarActivity {
+    public static final float CURRENT_VERSION = 0.9f;
     ListView listView;
 
     @Override
@@ -30,7 +44,7 @@ public class MenuActivity extends ActionBarActivity {
                 /*getString(R.string.Login), */
                 getString(R.string.Cards), getString(R.string.Idols),
                 getString(R.string.Events), getString(R.string.Contest), getString(R.string.Players),
-                getString(R.string.ContactUs), "???"
+                getString(R.string.ContactUs), getString(R.string.CheckForUpdates), "???"
         };
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 R.layout.menu_list_item, android.R.id.text1, options);
@@ -80,6 +94,10 @@ public class MenuActivity extends ActionBarActivity {
                         alertDialog.show();
                         break;
                     case 6:
+                        UpdateChecker uc = new UpdateChecker();
+                        uc.execute();
+                        break;
+                    case 7:
                         Toast toast = Toast.makeText(getApplicationContext(), "Be patient, we'll develop this soon...", Toast.LENGTH_SHORT);
                         toast.show();
                         break; // go to ...
@@ -117,5 +135,76 @@ public class MenuActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Class used to check if there are any pending updates.
+     */
+    private class UpdateChecker extends AsyncTask<Void, Void, Void> {
+        private Toast downloadingToast;
+        private Toast noUpdatesToast;
+        private Toast failToast;
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            downloadingToast = Toast.makeText(getApplicationContext(), R.string.DownloadingUpdate, Toast.LENGTH_LONG);
+            noUpdatesToast = Toast.makeText(getApplicationContext(), getString(R.string.NoUpdates) + " v" + CURRENT_VERSION, Toast.LENGTH_LONG);
+            failToast = Toast.makeText(getApplicationContext(), R.string.ConnectionFailed, Toast.LENGTH_LONG);
+            progressDialog = new ProgressDialog(MenuActivity.this);
+            progressDialog.setMessage(getString(R.string.CheckingUpdates));
+            progressDialog.setIndeterminate(true);
+            progressDialog.show();
+        }
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Preparing the URL used to get the current app version:
+                URL url = new URL("http://schoolido.lu/api/app/android/");
+
+                // Stablishing connection:
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+
+                // Reading the response:
+                // System.out.println("Response Code: " + conn.getResponseCode());
+                InputStream in = new BufferedInputStream(conn.getInputStream());
+                // TODO find why this doesn't work.
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+                String response = bufferedReader.readLine().trim();
+                JSONObject jsonObject = new JSONObject(response);
+                double newVersion = jsonObject.getDouble("version");
+                if (newVersion > CURRENT_VERSION) {
+                    // If there is a new version, we cancel the dialog and show the toast:
+                    downloadingToast.show();
+                    progressDialog.cancel();
+                    // Then, we construct the URI for the download link...
+                    String downloadUrl = jsonObject.getString("download_link").trim();
+                    // ... and pass it to the download manager request:
+                    DownloadManager.Request downloadRequest = new DownloadManager.Request(Uri.parse(downloadUrl));
+
+                    // We prepare the download and add it to the queue.
+                    downloadRequest.setDescription(getString(R.string.DownloadingUpdate) + " Sukutomo v" + newVersion);
+                    DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                    downloadManager.enqueue(downloadRequest);
+                } else {
+                    noUpdatesToast.show();
+                    progressDialog.cancel();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                failToast.show();
+                progressDialog.cancel();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Void v) {
+
+        }
     }
 }
